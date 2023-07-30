49533e9d10c687eeccad703ccff442be1a574ca8
ALERA0 <erenalp0103@gmail.com>
2023-07-30 16:58:03 +0300

    ürün girişinden ürün sil updated

--- routes/incomingProduct.js
+++ routes/incomingProduct.js
@@ -75,56 +75,54 @@ router.post("/addProductToIncomingProduct", upload.none(), async (req, res) => {
 });
 
 // Gelen belgesindeki ürünlerin adedini güncelle
-router.post(
-  "/updateIncomingProductQuantity",
-  upload.none(),
-  async (req, res) => {
-    try {
-      const { incomingProductId, rowId, newQuantity } = req.body;
+router.post("/removeProduct", upload.none(), async (req, res) => {
+  try {
+    const { incomingProductId, rowId } = req.body;
 
-      const data = await IncomingProduct.findById(incomingProductId);
-      if (!data) {
-        throw new Error("Güncellenecek ürün girişi bulunamadı");
-      }
-
-      const existingProduct = data.products.find(
-        (item) => item._id.toString() === rowId
-      );
-      if (!existingProduct) {
-        throw new Error("Ürün girişi bulunamadı");
-      }
-
-      const parsedNewQuantity = parseInt(newQuantity, 10);
-      if (isNaN(parsedNewQuantity) || parsedNewQuantity <= 0) {
-        throw new Error("Yeni quantity değeri geçerli bir sayı değil");
-      }
-
-      // Eski quantity değerini bulup, farkını hesapla
-      const diffQuantity = parsedNewQuantity - existingProduct.quantity;
-
-      // IncomingProduct modelindeki quantity değerini güncelle
-      existingProduct.quantity = parsedNewQuantity;
-
-      // Product modelindeki quantity değerini güncelle
-      const foundProduct = await Product.findById(existingProduct.product);
-      if (!foundProduct) {
-        throw new Error("Ürün bulunamadı");
-      }
-      foundProduct.productQuantity += diffQuantity;
-      await foundProduct.save();
-
-      await data.save();
-
-      res.status(200).json({
-        status: "success",
-        message: "Ürün girişi başarıyla güncellendi.",
-        data,
-      });
-    } catch (error) {
-      res.status(500).json({ status: "error", message: error.message });
+    // Mevcut ürün girişini bulma
+    const incomingProduct = await IncomingProduct.findById(incomingProductId);
+    if (!incomingProduct) {
+      throw new Error("Güncellenecek ürün girişi bulunamadı");
     }
+
+    // Çıkarılacak ürünü bul ve quantity değerini al
+    const productToRemove = incomingProduct.products.find(
+      (product) => product._id.toString() === rowId
+    );
+
+    if (!productToRemove) {
+      throw new Error("Çıkarılacak ürün listede bulunamadı");
+    }
+
+    const removedQuantity = productToRemove.quantity;
+
+    // Ürünleri _id değerine göre filtrele ve rowId değerine sahip olanı çıkar
+    incomingProduct.products = incomingProduct.products.filter(
+      (product) => product._id.toString() !== rowId
+    );
+
+    // Çıkarılan ürünün quantity değerini Product modelinde azalt
+    const foundProduct = await Product.findById(productToRemove.product);
+    if (foundProduct) {
+      foundProduct.productQuantity -= removedQuantity;
+      await foundProduct.save();
+    } else {
+      throw new Error("Çıkarılacak ürünün veritabanında kaydı bulunamadı");
+    }
+
+    // incomingProduct'i güncelle
+    await incomingProduct.save();
+
+    res.status(200).json({
+      status: "success",
+      message: "Ürün başarıyla çıkartıldı.",
+      incomingProduct,
+    });
+  } catch (error) {
+    res.status(500).json({ status: "error", message: error.message });
   }
-);
+});
+
 
 // Girilen ürünleri güncelleme
 router.post("/updateIncomingProduct", upload.none(), async (req, res) => {
